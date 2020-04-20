cc1a66c9c8328fc576f2faac302a8ed8bed41c9c
PratikDey <56970651+PratikDey@users.noreply.github.com>
2020-04-20 12:36:49 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import marked from 'marked';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    text: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+
+  render() {
+
+    const markdown = marked(this.state.text, { breaks: true })
+
+    return (
+      <div>
+        <h2 className="text-center m-4 text-white">Convert Your Markdown</h2>
+        <div className="row">
+          <div className="col-6">
+            <h6 className="text-center text-white">Enter your markdown:</h6>
+            <textarea className="form-control p-2" id="editor" value={this.state.text} onChange={this.handleChange}></textarea>
+          </div>
+          <div className="col-6" >
+            <h6 className="text-center text-white">See the result:</h6>
+            <div className="preview rounded p-2" id="preview" dangerouslySetInnerHTML={{__html: markdown}}>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App class="container-fluid"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
